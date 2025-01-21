2f72753aa75ff601f60c2e119023624fba770202
odaju <142748592+odaju@users.noreply.github.com>
2025-01-21 12:07:02 +0100

    lagrer ny kode

---
+++ Arbeidskrav_1/menu.js
@@ -0,0 +1,24 @@
+/**console.log(faneid)
+
+let faneHTML = ""
+
+resources.map((faneid, index) => faneHTML += 
+            `<ul class="faneid">
+                <li><a href=${fanid.resources}>HTML</a></li>
+                <li><a href=${faneid.resources}>CSS</a></li>
+                <li><a href=${faneid.resources}>JavaScript</a></li>
+                <li><a href=${faneid.resources}>React</a></li>
+                <li><a href=${faneid.resources}>Sanity and headless</a></li>
+            </ul>`)
+
+document.getElementById("faneid").innerHTML = faneHTML **/
+
+
+
+let faneHTML = ""
+
+resources.map((cat, index) => {
+    faneHTML += `<button>${cat.category}</button>`
+})
+
+document.getElementById("faneid").innerHTML = faneHTML
